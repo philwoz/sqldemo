34765e6076bca29c2fcaeb877b22e21b6c9354d7
philwoz <61164019+philwoz@users.noreply.github.com>
2021-04-15 10:34:25 +0100

    updated lidsql.js

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const SQL = require("./lib/sql");
+const SQL = require("./lib/libsql");
 
 console.log('Hello world, gonna try database stuff!');
 
 /* There's no place like 127.0.0.1 */
-const db = new SQL("127.0.0.1", "root", "PASSWORD", "cactus");
+const db = new SQL("127.0.0.1", "root", "PASSWORD", "db");
 db.select((error, results) => {
     if (error) {
         throw error
     } else {
         for (let result of results) {
-            console.log(`User is called: ${result.name} and they are ${result.gender}`);
+            console.log(`User is called: ${result.Name} and they are ${result.gender}`);
         }
         process.exit();
     }
