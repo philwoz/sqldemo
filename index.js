ca81a1bd47d826e7256eb8aca21fbe17e0cb79bd
philwoz <61164019+philwoz@users.noreply.github.com>
2021-04-15 11:40:34 +0100

    added update and create functions

--- index.js
+++ index.js
@@ -4,13 +4,27 @@ console.log('Hello world, gonna try database stuff!');
 
 /* There's no place like 127.0.0.1 */
 const db = new SQL("127.0.0.1", "root", "PASSWORD", "db");
-db.select((error, results) => {
-    if (error) {
-        throw error
-    } else {
-        for (let result of results) {
-            console.log(`User is called: ${result.Name} and they are ${result.gender}`);
-        }
-        process.exit();
-    }
-});
+// db.select((error, results) => {
+//     if (error) {
+//         throw error
+//     } else {
+//         for (let result of results) {
+//             console.log(`User is called: ${result.Name} and they are ${result.gender}`);
+//         }
+//         process.exit();
+//     }
+// }, "Laura");
+
+// db.create((error, results) => {
+//     if (error) throw error;
+//     console.log(results);
+//     process.exit();
+// }, ['1000','Developer', 20991232, 20991252 ])
+
+db.update((error, results) => {
+    if (error) throw error;
+    console.log(results);
+
+}, "Senior Developer", 1002)
+
+
